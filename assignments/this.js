4d7151ff09fe64da57be044b6b8304964cab2a47
ropeks <ropeksmilioner@gmail.com>
2019-05-08 13:14:43 +0200

    explain the four principles of this keyword

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding is when "this" keyword refers to the window Object.
+* 2. Implicit Binding is when "this" keyword refers to the object before the dot in that function call.
+* 3. New Binding is when "this" kw refers to the Object created by calling the constructor function with "new" kw.
+* 4. Explicit Binding is when "this" kw is explicitly defined while using call or apply method.
 *
 * write out a code example of each explanation above
 */
